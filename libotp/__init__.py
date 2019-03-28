1e76797170e38fc8abaee3d62750a92062b68aeb
John Cote <jwcotejr@gmail.com>
2019-03-28 00:41:58 -0400

    libotp: cleanup

--- libotp/__init__.py
+++ libotp/__init__.py
@@ -1,3 +1,4 @@
+import NametagGlobals
 from ChatBalloon import ChatBalloon
 from ClickablePopup import ClickablePopup
 from MarginManager import MarginManager
@@ -5,10 +6,8 @@ from MarginPopup import MarginPopup
 from Nametag import Nametag
 from Nametag2d import Nametag2d
 from Nametag3d import Nametag3d
-from NametagGroup import NametagGroup
-from WhisperPopup import WhisperPopup
 from NametagFloat2d import NametagFloat2d
 from NametagFloat3d import NametagFloat3d
+from NametagGroup import NametagGroup
+from WhisperPopup import WhisperPopup
 from _constants import *
-
-import NametagGlobals
